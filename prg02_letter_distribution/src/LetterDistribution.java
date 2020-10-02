2199a12b6e91f7931be43c858f78779df4d4d175
Thyago Mota <tmota@msudenver.edu>
2020-10-01 20:16:47 -0600

    updates

---
+++ prg02_letter_distribution/src/LetterDistribution.java
@@ -0,0 +1,24 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Prg02 - LetterDistribution
+ * Student(s) Name(s):
+ */
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+
+public class LetterDistribution {
+
+    static final String LETTERS_IN_THE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    static final char   FIRST_LETTER_CODE       = 'a';
+    static final String FILE_NAME               = "Constitution.txt";
+    static final int    FRAME_WIDTH             = 1024;
+    static final int    FRAME_HEIGHT            = 768;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        
+    }
+}
