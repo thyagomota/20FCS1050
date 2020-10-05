5b8c2210a07d105b76b726ea8e0c433cdd4463c9
Thyago Mota <tmota@msudenver.edu>
2020-10-05 09:56:32 -0600

    updates

---
+++ activity_20_students_average/src/StudentsAverage.java
@@ -0,0 +1,15 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 20 - StudentsAverage
+ */
+
+public class StudentsAverage {
+
+    static final String FILE_NAME = "data.csv";
+
+    // TODO: ask the user for a course number; then compute and display the average of the course using data from FILE_NAME
+    public static void main(String[] args) {
+
+    }
+}
