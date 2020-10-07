fe98226d4a1053f50bb2690533fa06cba48d5a8f
Thyago Mota <tmota@msudenver.edu>
2020-10-07 09:34:28 -0600

    updates

---
+++ activity_21_traffic_accidents/src/TrafficAccidents.java
@@ -0,0 +1,16 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 21 - TrafficAccidents
+ */
+
+public class TrafficAccidents {
+
+    static final String FILE_NAME = "traffic_accidents.csv";
+
+    // TODO: create and display a dynamic array of all types of traffic accidents based on data file obtained from FILE_NAME
+    public static void main(String[] args) {
+
+
+    }
+}
