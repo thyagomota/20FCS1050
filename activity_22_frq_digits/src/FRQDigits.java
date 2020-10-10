6418a203dab753e2bb0857a1403114e37ca6ab7b
Thyago Mota <tmota@msudenver.edu>
2020-10-10 11:52:37 -0600

    updates

---
+++ activity_22_frq_digits/src/FRQDigits.java
@@ -0,0 +1,31 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 22 - FRQDigits
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class FRQDigits {
+
+    public static void main(String[] args) {
+
+        // TODO #1: ask the user for an integer number > 0
+
+
+        // TODO #2: declare and initialize an ArrayList of Integer called "digitsList"
+
+
+        // TODO #3: add the digits from the given number into "digitsList"
+
+
+        // TODO #4: reverse "digitsList"
+
+
+        // TODO #5: print "digitsList"
+
+    }
+}
