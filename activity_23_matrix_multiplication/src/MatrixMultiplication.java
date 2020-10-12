a790b5739bcda68dd0856d52be32d4bb7f8f516e
Thyago Mota <tmota@msudenver.edu>
2020-10-12 09:59:00 -0600

    updates

---
+++ activity_23_matrix_multiplication/src/MatrixMultiplication.java
@@ -0,0 +1,29 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 23 - MatrixMultiplication
+ */
+
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+
+        // TO-DO #1: read the first matrix A
+
+
+        // TO-DO #2: read the second matrix B
+
+
+        // TO-DO #3: initialize matrix C
+
+
+        // TO-DO #4: multiply A x B
+
+
+        // TO-DO #5: display matrix C
+        
+        }
+    }
+}
