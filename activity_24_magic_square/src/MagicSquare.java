1f57793993d652b3d070e428ddb88ced0438869d
Thyago Mota <tmota@msudenver.edu>
2020-10-14 10:10:42 -0600

    updates

---
+++ activity_24_magic_square/src/MagicSquare.java
@@ -0,0 +1,12 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 24 - MagicSquaree
+ */
+
+public class MagicSquare {
+
+    public static void main(String[] args) {
+        
+    }
+}
