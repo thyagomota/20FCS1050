32dd50d6142b95d55015bf643f12d0c34e84805e
Thyago Mota <tmota@msudenver.edu>
2020-10-19 13:31:40 -0600

    updates

---
+++ activity_27_gcd_vr2/src/GCD.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 27 - GCD
+ */
+public class GCD {
+
+    // TODO: compute the gcd of given numbers a and b using the recursive definition 
+    static int gcd(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a? ");
+        int a = sc.nextInt();
+        System.out.print("b? ");
+        int b = sc.nextInt();
+        System.out.println("GCD(" + a + ", " + b + ") = " + gcd(a, b));
+    }
+}
