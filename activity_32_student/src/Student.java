a0c8cf2abd900547c941d4dde4d920e4ef158a26
Thyago Mota <tmota@msudenver.edu>
2020-11-09 09:06:40 -0700

    updates

---
+++ activity_32_student/src/Student.java
@@ -0,0 +1,25 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 33 - Student
+ */
+
+public class Student
+{
+    // TODO: Write 4 instance variables
+
+
+    // TODO: Write 3 constructors to initialize the instance variables
+    //  1. no parameters using default values
+    //  2. 1 parameter and the rest default values
+    //  3. 4 parameters
+
+
+    // TODO: override the toString method
+    public void print()
+    {
+
+
+    }
+}
+
---
+++ activity_32_student/src/StudentDriver.java
@@ -0,0 +1,18 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 33 - StudentDriver
+ */
+
+public class StudentDriver {
+
+    // main method
+    public static void main(String[] args)
+    {
+        // TODO: Construct 3 Student objects using the 3 different constructors
+
+
+        // TODO: call their toString() methods
+
+    }
+}
