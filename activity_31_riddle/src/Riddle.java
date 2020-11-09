106226d5e303a528f74a7cefc3d42688ccc0ded0
Thyago Mota <tmota@msudenver.edu>
2020-11-09 08:56:09 -0700

    updates

---
+++ activity_31_riddle/src/Riddle.java
@@ -0,0 +1,41 @@
+/*
+ * CS1050 - Computer Science I - Fall 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 31 - Riddle
+ */
+
+public class Riddle
+{
+    // write 2 instance variables for Riddle's question and answer: private type variableName;
+
+
+    // constructor
+    public Riddle(String initQuestion, String initAnswer)
+    {
+        // set the instance variables to the init parameter variables
+
+    }
+
+    // Print riddle question
+    public void printQuestion()
+    {
+        // print out the riddle question with System.out.println
+
+    }
+
+    // Print riddle answer
+    public void printAnswer()
+    {
+        // print out the riddle answer with System.out.println
+
+    }
+
+    // main method for testing
+    public static void main(String[] args)
+    {
+        // call the constructor to create 2 new Riddle objects
+
+        // call their printQuestion() and printAnswer methods
+
+    }
+}
